c7e5602cb98fa2953a7aa1314169c1ac9bfc21e5
EllieWalsch <elliewalsch4@gmail.com>
2023-03-14 19:33:10 -0500

    Add classwork

--- server/app/thought/index.js
+++ server/app/thought/index.js
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 import formatDateTime from "../format-date-time.js";
 import CommentSchema from "./comment-schema.js";
 
@@ -31,4 +31,4 @@ const ThoughtSchema = new Schema(
   }
 );
 
-export default ThoughtSchema;
+export default model("Thought", ThoughtSchema);
